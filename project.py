7a6b6991f8ff42156bd9230c9f94c3f28515e7e4
Sanjib Katuwal <katsanjib046@gmail.com>
2022-10-29 19:56:16 -0400

    it works for an arbitrary n-gram now

--- project.py
+++ project.py
@@ -95,10 +95,10 @@ def chunker(seq, size):
     Output:
         chunk -- Chunk
     """
-    return [''.join(seq[pos:pos + size]) for pos in range(0, len(seq), size)]
+    return [''.join(seq[pos:pos + size]) for pos in range(0, len(seq)-size)]
 
 
-def count_matrix(file=None, text=None):
+def count_matrix(file=None, text=None, n_gram=2):
     """
     Function: 
         To count the number of times a letter is followed by another letter in a given text file.
@@ -119,141 +119,15 @@ def count_matrix(file=None, text=None):
         text = text.lower()
 
     # create a list of all the letters in the text
-    letters_list = [letter for letter in text if letter.isalpha() or letter == ' ']
+    letters_list = [letter.lower() for letter in text if letter.isalpha() or letter == ' ']
 
     # make chuncks of 2 letters
-    chunks = chunker(letters_list, 2)
+    chunks = chunker(letters_list, n_gram)
 
     # create a dictionary of all the letters and their counts
     count_dict = c.Counter(chunks)
 
     return count_dict
-
-
-def count_matrix_trigram(file=None, text=None):
-    """
-    Function: To get the count matrix for trigram.
-    Input:
-        file -- File name
-        text -- Text
-    Output:
-        count_matrix -- Count matrix
-    """
-    # letters, alphabet
-    letters = list(LETTERS)
-    # Get the count matrix
-    count_matrix = np.zeros((len(letters), len(letters), len(letters)))
-    if file:
-        with open(file, 'r', encoding='utf8') as f:
-            text = f.read().lower()
-    for i in range(len(text)-2):
-        # Get the current letter and the next letter
-        current_letter = text[i]
-        next_letter = text[i + 1]
-        next_next_letter = text[i + 2]
-        # Get the index of the current letter and the next letter
-        if current_letter in letters and next_letter in letters and next_next_letter in letters:
-            current_index = letters.index(current_letter)
-            next_index = letters.index(next_letter)
-            next_next_index = letters.index(next_next_letter)
-            # Increment the count
-            count_matrix[current_index, next_index, next_next_index] += 1
-    return count_matrix
-
-
-def count_matrix_unigram(file=None, text=None):
-    """
-    Function: To get the count matrix for unigram.
-    Input:
-        file -- File name
-        text -- Text
-    Output:
-        count_matrix -- Count matrix
-    """
-    # letters, alphabet
-    letters = list(string.ascii_lowercase)
-    # Get the count matrix
-    count_matrix = np.zeros((len(letters)))
-    if file:
-        with open(file, 'r', encoding='utf8') as f:
-            text = f.read().lower()
-    for i in range(len(text)):
-        # Get the current letter and the next letter
-        current_letter = text[i]
-        # Get the index of the current letter and the next letter
-        if current_letter in letters:
-            current_index = letters.index(current_letter)
-            # Increment the count
-            count_matrix[current_index] += 1
-    return count_matrix
-
-
-def unigram_attack(cipher_text):
-    """
-    Function: To get the unigram attack.
-    Input:
-        cipher_text -- Cipher text
-    Output:
-        key -- Key
-    """
-    # Get the count matrix
-    count_matrix = count_matrix_unigram(file='wp.txt')
-    # Get the count matrix
-    count_matrix_message = count_matrix_unigram(text=cipher_text)
-    # Get the key
-    key = get_key_unigram(count_matrix, count_matrix_message)
-    return key
-
-
-def get_key_unigram(count_matrix, count_matrix_message):
-    """
-    Function: To get the key for unigram attack.
-    Input:
-        count_matrix -- Count matrix
-        count_matrix_message -- Count matrix for the message
-    Output:
-        key -- Key
-    """
-    # letters, alphabet
-    letters = list(string.ascii_uppercase)
-    # Get the key
-    key = [i for i in range(len(letters))]
-    # iterate over the count_matrix_message
-    for i in range(len(count_matrix_message)):
-        # Get the index of the maximum value in the count matrix message
-        index_message = np.argmax(count_matrix_message)
-        # get the corresponding letter
-        letter_message = letters[index_message]
-        # Get the index of the maximum value in the count matrix
-        index_count_matrix = np.argmax(count_matrix)
-        # Get the letter corresponding to the index
-        letter_matrix = letters[index_count_matrix]
-        # make replacement in the key
-        key[index_message] = letter_matrix
-        # Make the count matrix message and count matrix zero
-        count_matrix_message[index_message] = -(i+1)
-        count_matrix[index_count_matrix] = -(i+1)
-    key = ''.join(key)
-    return key
-
-
-def probability_matrix(count_matrix):
-    """
-    Function: 
-        To calculate the probability matrix.
-    Input:
-        count_matrix -- Count matrix
-    Output: 
-        probability_matrix -- Probability matrix
-    """
-    # Get the sum of each row
-    row_sums = count_matrix.sum(axis=1)
-
-    # Divide each row by the sum of the row
-    probability_matrix = count_matrix / row_sums[:, np.newaxis]
-
-    # Return the probability matrix
-    return probability_matrix
 
 
 def random_key():
@@ -346,27 +220,13 @@ def get_score(plain_text, probMatrix, n_gram=2):
     # letters, alphabet
     letters = list(LETTERS)
     score = 0
+    currentMatrix = count_matrix(text=plain_text, n_gram=n_gram)
+    for key in currentMatrix.keys():
+        if key in probMatrix.keys():
+            score += (1+currentMatrix[key]) * np.log(1+probMatrix[key])
 
-    if n_gram == 2:
-        currentMatrix = count_matrix(text=plain_text)
-        for key in currentMatrix.keys():
-            if key in probMatrix.keys():
-                score += (1+currentMatrix[key]) * np.log(1+probMatrix[key])
-
-    elif n_gram == 3:
-        currentMatrix = count_matrix_trigram(text=plain_text)
-        for i in range(len(plain_text)-2):
-            # Get the current letter and the next letter
-            current_letter = plain_text[i]
-            next_letter = plain_text[i + 1]
-            third_letter = plain_text[i + 2]
-            # Get the index of the current letter and the next letter
-            current_index = letters.index(current_letter)
-            next_index = letters.index(next_letter)
-            third_index = letters.index(third_letter)
-            # Increment the score
-            score += (1+currentMatrix[current_index, next_index, third_index]) * np.log(1+probMatrix[current_index, next_index, third_index])
     return score
+
 
 def get_new_key(key):
     """
@@ -402,10 +262,7 @@ def mcmc(cipher_text, message, n_gram=2):
     """
     # get the count matrix for reference text
     print("Getting the count matrix for reference text...")
-    if n_gram == 2:
-        countMatrix = count_matrix(file='wp.txt')
-    elif n_gram == 3:
-        countMatrix = count_matrix_trigram(file='wp.txt')
+    countMatrix = count_matrix(file='wp.txt', n_gram=n_gram)
     print("Count matrix for reference text is ready.")
     # regulating temperature
     Tmax = 1000
@@ -423,7 +280,7 @@ def mcmc(cipher_text, message, n_gram=2):
     # get a random key
     # key = random_key()
     # get a key using unigram
-    key = unigram_attack(cipher_text)
+    key = random_key()
 
     # get the plain text
     plain_text = decrypt(cipher_text, key)
@@ -495,19 +352,6 @@ def plot_score(info, file_name):
     plt.ylabel('Score')
     plt.savefig(file_name + '_score.png')
 
-def matrix_info(matrix):
-    """
-    Function: To print the matrix information.
-    Input:
-        matrix -- Matrix
-    Output:
-        None
-    """
-    # Print the matrix information
-    print('Matrix shape: ', matrix.shape)
-    print('Matrix sum: ', matrix.sum())
-    print('Matrix max: ', matrix.max())
-    print('Matrix min: ', matrix.min())
 
 
 def accuracy(given_text, predicted_text):
@@ -527,6 +371,7 @@ def accuracy(given_text, predicted_text):
     accuracy = accuracy / len(given_text)
     return accuracy
 
+
 def write_output(message, info, file_name, time_taken, n_gram):
     """
     Function: To write the output to a file.
@@ -539,7 +384,7 @@ def write_output(message, info, file_name, time_taken, n_gram):
         None
     """
     # Write the output to a file
-    with open(file_name, 'w') as f:
+    with open(file_name, 'a+') as f:
         f.write('message: ' + message + '\n')
         f.write('time taken: ' + str(time_taken) + '\n')
         f.write('n_gram: ' + str(n_gram) + '\n')
@@ -551,6 +396,7 @@ def write_output(message, info, file_name, time_taken, n_gram):
             f.write(str(info[keys]['score']) + ',')
             f.write(str(info[keys]['accuracy'])+ ',')
             f.write(info[keys]['plain_text'])
+        f.write('\n')
 
 
 # ------------------ Testing and Running ------------------
